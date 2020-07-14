14728e70b7ac0b62b5b848bc31e03e3c0d22fcbd
Abhilash Sulibela <abhilash1869@gmail.com>
2020-07-14 16:29:03 -0500

    feat(module-client): Add nav bar & hide private routes

--- fron-end/onboard-client/src/app/auth/auth.service.ts
+++ fron-end/onboard-client/src/app/auth/auth.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http"
 import { RegisterUserRequest } from "./model/register-user-request"
 import { RegisterUserResponse } from "./model/register-user-response"
+import { BehaviorSubject } from "rxjs"
+import { tap } from "rxjs/operators"
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   rootUrl = "http://localhost:3000/api/v1/auth"
+  signedIn$ = new BehaviorSubject(false);
+
   constructor(private http: HttpClient) { }
 
   register(registerUserRequest: RegisterUserRequest) {
-    return this.http.post<RegisterUserResponse>(`${this.rootUrl}/register`, registerUserRequest);
+    return this.http.post<RegisterUserResponse>(`${this.rootUrl}/register`, registerUserRequest)
+      .pipe(
+        tap(() => { this.signedIn$.next(true) }
+        )
+      );
   }
 }
