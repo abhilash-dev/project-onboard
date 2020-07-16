e4abc46a16ae72ded97b1c96fc02510ae3de478e
Abhilash Sulibela <abhilash1869@gmail.com>
2020-07-16 09:45:21 -0500

    refactor(module-client): add minor fixes

--- fron-end/onboard-client/src/app/projects/project-form/project-form.component.ts
+++ fron-end/onboard-client/src/app/projects/project-form/project-form.component.ts
@@ -39,7 +39,7 @@ export class ProjectFormComponent implements OnInit {
         [Validators.required, Validators.pattern(/^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/)]
       ),
       train_size: new FormControl(this.isNew ? '' : this.project.train_size,
-        [Validators.required]
+        [Validators.required, Validators.pattern(/^([,|.]?[0-9])+$/)]
       ),
       problem_type: new FormControl(this.isNew ? '' : this.project.problem_type,
         [Validators.required]
